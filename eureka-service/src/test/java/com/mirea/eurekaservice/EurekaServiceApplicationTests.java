94636772d37026d048ee788688d4c15d88f4c18f
LobanSel <lobdemon@mail.ru>
2024-03-15 22:40:46 +0300

    Added Eureka and parallel run for arsenal service

---
+++ eureka-service/src/test/java/com/mirea/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirea.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
