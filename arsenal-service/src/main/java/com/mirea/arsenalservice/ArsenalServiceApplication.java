ed09dabaff74fef3605b81e8189020e650e5fb63
LobanSel <lobdemon@mail.ru>
2024-03-15 22:27:16 +0300

    Added Eureka and parallel run for arsenal service

--- arsenal-service/src/main/java/com/mirea/arsenalservice/ArsenalServiceApplication.java
+++ arsenal-service/src/main/java/com/mirea/arsenalservice/ArsenalServiceApplication.java
@@ -5,9 +5,11 @@ import com.mirea.arsenalservice.repo.ArsenalRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ArsenalServiceApplication {
 
 	public static void main(String[] args) {
